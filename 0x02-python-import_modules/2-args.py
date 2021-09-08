ca15aad8fc9f84874c7d9b4285bd996b9d467fec
eylens <eymasac@gmail.com>
2021-09-08 18:29:26 -0500

    add new print to number of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 import sys
 
 if __name__ == "__main__":
-    numArguments = len(sys.argv)
-    if numArguments == 1:
+    numArguments = len(sys.argv) -1
+    if numArguments == 0:
         print("0 arguments.")
-    elif numArguments == 2:
+    elif numArguments == 1:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
-    elif numArguments > 2:
+    elif numArguments > 1:
+        print("{} arguments:".format(numArguments))
         for i in range(1, numArguments + 1):
             print("{} arguments: {}".format(i, sys.argv[i]))
