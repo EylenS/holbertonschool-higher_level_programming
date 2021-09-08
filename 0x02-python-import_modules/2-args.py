eb6aaa4cbd7d8086a33c30b558cb8b982e77a85b
eylens <eymasac@gmail.com>
2021-09-08 18:32:08 -0500

    correction:def main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == "__main__":
+def main():
     numArguments = len(sys.argv) -1
+    
     if numArguments == 0:
         print("0 arguments.")
     elif numArguments == 1:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
-    elif numArguments > 1:
+    else:
         print("{} arguments:".format(numArguments))
         for i in range(1, numArguments + 1):
             print("{} arguments: {}".format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    main()
