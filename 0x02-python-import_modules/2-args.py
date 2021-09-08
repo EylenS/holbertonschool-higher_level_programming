6d9aa9b9e668e1f63e7a921585dc85c359ddf0b1
eylens <eymasac@gmail.com>
2021-09-08 18:34:39 -0500

    change in 14 line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,16 +3,15 @@ import sys
 
 def main():
     numArguments = len(sys.argv) -1
-    
     if numArguments == 0:
         print("0 arguments.")
     elif numArguments == 1:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(numArguments))
+        print("{:d} arguments:".format(numArguments))
         for i in range(1, numArguments + 1):
-            print("{} arguments: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]))
 
 if __name__ == "__main__":
     main()
