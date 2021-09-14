f7f7fe222804a3e6f57833dc4b3f5a782e8d482d
eylens <eymasac@gmail.com>
2021-09-14 17:36:14 -0500

    2nd change

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
 from add_0 import add
-
-def main():
+if __name__ == "__main__":
     a = 1
     b = 2
     sum = add(a, b)
     print("{} + {} = {}".format(a, b, sum))
-
-if __name__ == "__main__":
-    main()
