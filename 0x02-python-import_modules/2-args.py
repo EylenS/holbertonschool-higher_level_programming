d1b2fb79b35bca8c18133f6ba30a6cf0d1630f90
eylens <eymasac@gmail.com>
2021-09-14 17:45:05 -0500

    3rd change_identation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    numArguments = len(sys.argv) -1
+    numArguments = len(sys.argv) - 1
     if numArguments == 0:
         print("0 arguments.")
     elif numArguments == 1:
