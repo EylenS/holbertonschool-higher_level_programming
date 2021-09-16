48fa3e3d6314e05e19f73f91327f4359cdf4b8c1
eylens <eymasac@gmail.com>
2021-09-15 23:50:04 -0500

    from roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return None
+    r_i = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    val_ant = 0
+    for idx in range(len(roman_string)):
+        for key, val in r_i.items():
+            if key == roman_string[idx]:
+                result += val
+                if val_ant < val:
+                    result -= (val_ant*2)
+                    pValue = val_ant
+    return result
+# val_ant: previous value
