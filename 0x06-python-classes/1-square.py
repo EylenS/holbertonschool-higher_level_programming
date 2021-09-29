217e107fb4359902f0e3592e0fc2fe12e7699f57
eylens <eymasac@gmail.com>
2021-09-28 21:39:51 -0500

    2nd commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,17 @@
 
 
 class Square:
-    """Square represents a square"""
-    def __init__(self, size):
-        """Constructor of class Square, initializes the data"""
+    """Square represents a square
+    
+    Attributes:
+        _size(int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """Initializes a square
+        
+        Args:
+            size(int)size of a side of the square
+
+        Returns: None
+        """
         self._size = size
