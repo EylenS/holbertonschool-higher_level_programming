37c7fc7017f0958c940a99058e1c9d7aa56b345e
eylens <eymasac@gmail.com>
2021-09-29 09:37:10 -0500

    new attribute private

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,15 +3,18 @@
 
 
 class Square:
+
     """
     This class represents a square
     Attributes:
         size(int): size of the square - private attribute
     """
 
-    def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self._size = size
+    def __init__(self, size):
+        
+        """Initializing the data
+        Arguments:
+            size: The size of a square
+        """
+
+        self.__size = size
