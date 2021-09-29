75f3539b1c4cca0e08e4b92efc9c694def4b417f
eylens <eymasac@gmail.com>
2021-09-29 10:35:54 -0500

    Calculates the area, ublic attribute

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Defines a class called Square"""
+
+
+class Square:
+
+    """
+    This class represents a square
+    Attributes:
+        size(int): size of the square - private attribute
+    """
+
+    def __init__(self, size=0):
+
+        """Instantiation with size
+        Argument:
+            __size: is private attribute
+        Raises:
+            TypeError: The value of size gotten is not am int
+            ValueError:The value gottenis less than 0
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
+    def area(self):
+
+        """Public instance method: area "a"
+            Calculates the area of square
+            and returns an actual squar area
+        """
+        a = self.__size**2
+        return a
