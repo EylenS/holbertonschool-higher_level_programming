da7f060692c851bb2ce418f65df04047d5d6baed
eylens <eymasac@gmail.com>
2021-09-28 21:33:31 -0500

    size private of the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class called Square"""
+
+
+class Square:
+    """Square represents a square"""
+    def __init__(self, size):
+        """Constructor of class Square, initializes the data"""
+        self._size = size
