92957d36d82aa2686c6f3a51135a557028b9f729
eylens <eymasac@gmail.com>
2021-09-30 10:47:56 -0500

    4th commit

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a rectangle class."""
+
+
 class Rectangle:
     """Represent a rectangle."""
     number_of_instances = 0
@@ -85,6 +88,7 @@ class Rectangle:
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
-        """Print a message when an instance of Rectangle is deleted."""
+        """Print a message
+        when an instance of Rectangle is deleted."""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
