cadaa0f772b8c89537d3f873543a16d9c902ca27
eylens <eymasac@gmail.com>
2021-10-06 11:55:25 -0500

    print a string

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function that prints a string."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints the string My name is <first name>
+    followed by the <last name>.
+    However, <last name> is optional."""
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
