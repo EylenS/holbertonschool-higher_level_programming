bc253584c45cc308316ed27f37e2c9526995336c
eylens <eymasac@gmail.com>
2021-10-06 14:57:15 -0500

    identation on a text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a function that changes the format of the text
+according to some characters founded."""
+
+
+def text_indentation(text):
+    """Based on the test passed,
+    prints the text with 2 new lines after each of these
+    characters: ., ? and :
+    On the other hand, there should be no space at the
+    beginning or at the end of each printed line.
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    text = text.strip()
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print("\n")
+            if i == len(text) - 1:
+                break
+            if text[i + 1] == " ":
+                i += 1
+            while text[i] == " " and text[i + 1] == " " and i + 1 < len(text):
+                i += 1
+        i += 1
