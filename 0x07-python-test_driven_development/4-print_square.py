a41965fc2603743edc3ba88520f053ea97d91163
eylens <eymasac@gmail.com>
2021-10-06 20:37:54 -0500

    print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,7 +5,7 @@
 def print_square(size):
     """Prints a square with the character '#'."""
 
-    if type(size) is not int:
+    if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
