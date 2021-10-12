5750a16b406b0b41645a8f7c4dcb04f2973af588
eylens <eymasac@gmail.com>
2021-10-12 11:24:06 -0500

    module be no implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an empty class."""
+
+
+class BaseGeometry:
+    """This class is empty."""
+
+    def area(self):
+        """This method hasn't been implemented yet."""
+        raise Exception("area() is not implemented")
