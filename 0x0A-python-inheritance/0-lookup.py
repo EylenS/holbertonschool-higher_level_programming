e2963a17a2ca2fa9b4260e6b9ea5045c097ecee8
EylenS <85471077+EylenS@users.noreply.github.com>
2021-10-12 14:07:31 -0500

    Delete 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Defines a function that look up attributes and methods.
-"""
-
-
-def lookup(obj):
-    """This method returns the list of attributes and methods."""
-    return dir(obj)
