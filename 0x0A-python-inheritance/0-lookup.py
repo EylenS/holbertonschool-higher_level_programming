e1a9875481743cc1de0159c0e9594b906188a026
eylens <eymasac@gmail.com>
2021-10-12 08:48:33 -0500

    5th commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-This module look up attributes and methods.
+Defines a function that look up attributes and methods.
 """
 
 
 def lookup(obj):
     """This method returns the list of attributes and methods."""
-
     return dir(obj)
