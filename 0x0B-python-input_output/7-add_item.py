36aa27b3d51f7ec21b97533cccd6b1eefdeb0057
eylens <eymasac@gmail.com>
2021-10-12 22:25:51 -0500

    Handle args and then save them in file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,10 +10,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 try:
-    my_list =load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
 except:
     my_list = []
 
 for args in argv[1:]:
-    my_list.append[args]
+    my_list.append(args)
 save_to_json_file(my_list, filename)
