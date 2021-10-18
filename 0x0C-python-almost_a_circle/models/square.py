2e498778d66a230e442992b6dabfcd37e8ee0533
eylens <eymasac@gmail.com>
2021-10-18 16:41:04 -0500

    getter and setter to square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,24 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """This class """
     def __init__(self, size, x=0, y=0, id=None):
-        """initialize an instance of a Square."""
+        """Initialize an instance of a Square."""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Overrides the __str__ method to return a predefined string."""
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """getter to obtain the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter to assign the width and the height(same value)."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
