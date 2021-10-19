7bd82b87babdb2ef4586579b01a8074e7be58ea9
eylens <eymasac@gmail.com>
2021-10-18 20:49:54 -0500

    27 line code modified

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,6 +23,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """This method returns the JSON string representation
         of list_dictionaries."""
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
-            return []
+        if len(list_dictionaries) == 0:
+            return "[]"
         return (json.dumps(list_dictionaries))
