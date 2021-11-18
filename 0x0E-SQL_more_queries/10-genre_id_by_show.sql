c9ed108e6dd01afe485a265999677b010fa25054
eylens <eymasac@gmail.com>
2021-11-18 11:46:51 -0500

    CHANGE LINE CODE 9: tv_show_genres.show.id

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,8 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
