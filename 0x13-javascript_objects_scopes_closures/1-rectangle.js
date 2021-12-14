070e97905a12af61db90d6038279866fafac7631
eylens <eymasac@gmail.com>
2021-12-14 16:49:58 -0500

    INitializes 2 attrs

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Define a class called Rectangle, INitializing 2 attr.
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
