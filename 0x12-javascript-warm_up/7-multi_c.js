9877b7a5abe7df670a679347ca5cbb2bb0894a49
eylens <eymasac@gmail.com>
2021-12-13 21:05:33 -0500

    print x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]);
 let i = 1;
-if (!parseInt(process.argv[2])) {
-  console.log('Missing number of occurrences');
-} else {
+if (parseInt(process.argv[2])) {
   if (x >= 0 && x > i) {
     for (i = 1; i <= x; i++) {
       console.log('C is fun');
     }
   }
+} else {
+  console.log('Missing number of occurrences');
 }
