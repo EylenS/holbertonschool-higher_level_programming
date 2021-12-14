6589b85f8715a332a5078280a9e295f7918edf27
eylens <eymasac@gmail.com>
2021-12-13 21:10:49 -0500

    4th commit

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]);
-let i = 1;
+let i = 0;
 if (parseInt(process.argv[2])) {
-  if (x > 0 && x > i) {
-    for (i = 1; i <= x; i++) {
+  if (x >= 0 && x > i) {
+    for (i = 0; i < x; i++) {
       console.log('C is fun');
     }
   }
