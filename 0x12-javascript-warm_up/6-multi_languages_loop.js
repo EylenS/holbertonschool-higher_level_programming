401ef299caf5fba157df047bc3826c359647b4f4
eylens <eymasac@gmail.com>
2021-12-13 20:23:00 -0500

    fix range to iterate

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i <= array.length; i++) {
+for (let i = 0; i <= array.length - 1; i++) {
   console.log(array[i]);
 }
