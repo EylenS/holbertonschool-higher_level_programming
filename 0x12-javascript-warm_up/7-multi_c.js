4b7fd1ca6c31d83f581ee6db516cc30c1a1ea6d3
eylens <eymasac@gmail.com>
2021-12-13 21:09:07 -0500

    print x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const x = parseInt(process.argv[2]);
 let i = 1;
 if (parseInt(process.argv[2])) {
-  if (x >= 0 && x > i) {
+  if (x > 0 && x > i) {
     for (i = 1; i <= x; i++) {
       console.log('C is fun');
     }
