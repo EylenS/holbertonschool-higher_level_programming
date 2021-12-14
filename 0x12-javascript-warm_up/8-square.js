97ce7b2c86752d3da5384af17d970ea3b7ff25b3
eylens <eymasac@gmail.com>
2021-12-13 21:38:05 -0500

    prints squarer

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (parseInt(process.argv[2])) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
