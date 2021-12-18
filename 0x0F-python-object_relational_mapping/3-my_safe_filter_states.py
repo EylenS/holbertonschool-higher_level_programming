d1df1c8397b38ff8a125dc5b7a672957c9eeda91
eylens <eymasac@gmail.com>
2021-12-18 13:46:03 -0500

    SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This scrips allows to connect with database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Connecting to MySQL database, getting a cursor and executing queries."""
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name like %s ORDER BY id ASC",
+        (argv[4], ))
+    query_states = cursor.fetchall()
+    for state in query_states:
+        print(state)
+    cursor.close()
+    db.close()
