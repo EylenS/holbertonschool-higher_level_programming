1bfb81bc5ff91d7686c986961d164a724641dfb8
eylens <eymasac@gmail.com>
2021-12-18 15:26:23 -0500

    filter cities according to state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This script takes in the name of a state as an argument and lists all
+cities of that state, using the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Connecting to MySQL database, getting a cursor and executing queries."""
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT cities.name FROM cities JOIN states\
+        ON cities.state_id = states.id WHERE states.name\
+        LIKE %s ORDER BY cities.id", (argv[4], ))
+    query = cursor.fetchall()
+
+    cities = []
+    idx = 0
+    for city in query:
+        cities.append(query[idx][0])
+        idx += 1
+    allCities = ', '.join(cities)
+    print(allCities)
+    cursor.close()
+    db.close()
