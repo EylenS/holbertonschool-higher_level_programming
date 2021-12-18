a8053f7d0660bd6971960e0435dcf2a67a8759ed
eylens <eymasac@gmail.com>
2021-12-18 13:28:41 -0500

    pycidestyle on line 13 and 14

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,8 @@ from sys import argv
 if __name__ == "__main__":
     """Connecting to MySQL database, getting a cursor and executing queries."""
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     query_states = cursor.fetchall()
