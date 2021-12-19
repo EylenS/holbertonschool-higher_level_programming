394e32e34de08ea40b624bc55ed4543b2e645b4d
eylens <eymasac@gmail.com>
2021-12-18 21:22:58 -0500

    filter by letter in name of the state

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa"""
+
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """to be accessed directly for MetaData-specific operations."""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id.asc()).all():
+        for letter in state.name:
+            if letter == 'a':
+                print("{}: {}".format(state.id, state.name))
+    session.close()
