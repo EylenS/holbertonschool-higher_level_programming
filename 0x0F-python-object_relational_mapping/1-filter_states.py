9dea994255802b2b521039b5e304df0ef2fc00b4
eylens <eymasac@gmail.com>
2021-12-20 10:24:31 -0500

    specific attr included, change LIKE to LIKE BINARY, and pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute(
+        """SELECT id, name FROM states WHERE name LIKE BINARY 'N%'
+        ORDER BY id ASC""")
     query_states = cursor.fetchall()
     for state in query_states:
         print(state)
