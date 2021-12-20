83e28ad09aaa3a01daf04ac302996fc9c4baa2fb
eylens <eymasac@gmail.com>
2021-12-20 10:14:09 -0500

    LIKE was in lower, it was change to upper

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This scrips allows to connect with database hbtn_0e_0_usa.
+This scrip  lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_states = cursor.fetchall()
     for state in query_states:
         print(state)
