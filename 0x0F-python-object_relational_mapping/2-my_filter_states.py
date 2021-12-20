72b038274a9d63820cac7c680f0382afa5baae47
eylens <eymasac@gmail.com>
2021-12-20 10:30:22 -0500

    Chanke LIKE to LIKE BINARY, and specific attr

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,9 +13,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name like '{:s}' ORDER BY id ASC".format(
-            argv[4]))
+    cursor.execute("""SELECT id, name FROM states WHERE name
+    LIKE BINARY '{:s}' ORDER BY id ASC""".format(argv[4]))
     query_states = cursor.fetchall()
     for state in query_states:
         print(state)
