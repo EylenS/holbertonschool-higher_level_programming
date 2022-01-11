515528e6707cfe4a1ce8ce694ac37ed5423ed6f2
eylens <eymasac@gmail.com>
2022-01-11 18:49:38 -0500

    identation

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 '''sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response ('''
 from urllib.request import urlopen, Request
-from urllib import parse 
+from urllib import parse
 from sys import argv
 
 
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     encoded_args = parse.urlencode(e_mail).encode('utf-8')
     req = Request(url, encoded_args)
     with urlopen(req) as response:
-         the_page = response.read()
+        the_page = response.read()
     print(urlopen(url, encoded_args).read().decode('utf-8'))
