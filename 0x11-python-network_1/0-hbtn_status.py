735cea9a5afd500866d92669507f228f944d075c
eylens <eymasac@gmail.com>
2022-01-11 16:57:34 -0500

    change '.' by ',' before type(htmml)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urlopen(url) as response:
         html = response.read()
         print('Body response:')
-        print('\t- type:'.type(html))
+        print('\t- type:', type(html))
         print('\t- content:', html)
         print('\t- utf8 content:', (html.decode('utf-8')))
