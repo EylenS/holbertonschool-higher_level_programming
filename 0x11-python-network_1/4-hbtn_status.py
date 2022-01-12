d7f7b8ad629599aa15e57f44d116cc8e7119b752
eylens <eymasac@gmail.com>
2022-01-11 21:23:08 -0500

    headers dictionary

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,9 +3,10 @@
 import requests
 
 
-url = 'https://intranet.hbtn.io/status'
-req = requests.get(url)
-r = req.text
-print('Body response:')
-print('\t- type:', type(r))
-print('\t- content:', r)
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    req = requests.get(url)
+    r = req.text
+    print('Body response:')
+    print('\t- type:', type(r))
+    print('\t- content:', r)
