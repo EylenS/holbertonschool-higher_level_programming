55520ecceae2ec7a7bd3ec11b7d45fa6e5482248
eylens <eymasac@gmail.com>
2022-02-01 16:23:11 -0500

    indentation adjusted

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,10 @@
 /* This script let reads and prints the content of a file */
 const fs = require('fs');
 const content = process.argv[2];
-fs.readFile(content, 'utf-8', function(error, data) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(data);
-    }
+fs.readFile(content, 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
