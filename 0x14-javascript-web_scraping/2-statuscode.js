81fcc85b96a1a85deb6238d12ec03f59fedf9c96
eylens <eymasac@gmail.com>
2022-02-01 17:37:50 -0500

    include GET

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 /* This script let display the status code of a GET request. */
 const request = require('request');
 const url = process.argv[2];
-request(url, function (error, response, body) {
+request.get(url, function (error, response, body) {
   if (error) {
     console.log(error);
   }
