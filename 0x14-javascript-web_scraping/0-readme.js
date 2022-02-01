3898d6613410b9d2b2f10e4ce5b1633075b9b278
eylens <eymasac@gmail.com>
2022-02-01 16:11:51 -0500

    include process module

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* This script let reads and prints the content of a file */
 var fs = require('fs');
+var process = require('process');
 var content = fs.readFile(process.argv[2], 'utf-8', function(error, data){
     if(error) {
         console.log(error);
