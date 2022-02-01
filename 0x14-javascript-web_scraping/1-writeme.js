fe534d3cf067e256b07c17420a86aa94f8badb2a
eylens <eymasac@gmail.com>
2022-02-01 16:49:44 -0500

    write file with fs module

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* This script let writes a string to a file. */
+const fs = require('fs');
+const path = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(path, string, 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
