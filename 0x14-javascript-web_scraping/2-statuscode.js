c7cc3f8d63962552bacaefb2e4a2762d07390a3b
eylens <eymasac@gmail.com>
2022-02-01 17:44:42 -0500

    adding var options

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,11 @@
 /* This script let display the status code of a GET request. */
 const request = require('request');
 const url = process.argv[2];
-request.get(url, function (error, response, body) {
+const options = {
+  url, method: 'GET'
+}
+
+request(options, function (error, response) {
   if (error) {
     console.log(error);
   }
