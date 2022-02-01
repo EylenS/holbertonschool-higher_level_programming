16902ebce44944e15352be8c97a73ba27d0e1255
eylens <eymasac@gmail.com>
2022-02-01 16:18:55 -0500

    storing the path into the variable, and change type variables

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* This script let reads and prints the content of a file */
-var fs = require('fs');
-var process = require('process');
-var content = fs.readFile(process.argv[2], 'utf-8', function(error, data){
-    if(error) {
+const fs = require('fs');
+const content = process.argv[2];
+fs.readFile(content, 'utf-8', function(error, data) {
+    if (error) {
         console.log(error);
     } else {
         console.log(data);
