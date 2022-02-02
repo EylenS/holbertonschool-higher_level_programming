d957ec671da4c4b8fe9ad991d3c0a8c861964203
eylens <eymasac@gmail.com>
2022-02-01 20:12:50 -0500

    trying with option var

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,17 +2,18 @@
 /* This script let prints the title of a Star Wars movie where
 the episode number matches a given integer. */
 const request = require('request');
-const episodeN = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + episodeN;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const options = {
   url,
   method: 'GET'
 };
 
-request(options, function (error, body) {
+request(options, function (error, response, body) {
   if (error) {
     console.log(error);
+  } else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.log('status code:', response && response.statusCode);
   }
-  const movieJSON = JSON.parse(body);
-  console.log(movieJSON.title);
 });
